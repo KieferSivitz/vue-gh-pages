2f4e48d27deb824f30987cf915a18188dbb8a22b
kiefersivitz <kmsivitz@gmail.com>
2017-08-29 16:50:47 -0400

    Copying 404.html to docs folder if it exists

--- index.js
+++ index.js
@@ -4,40 +4,50 @@ var fs = require('fs');
 var exec = require('child_process').exec;
 var rimraf = require('rimraf');
 
-function copyCNAME () {
-    ncp('CNAME', 'docs/CNAME', function (err)      {
+function copy404 () {
+    ncp('404.html', 'docs/404.html', function (err) {
         if (err) {
             return console.error(err);
         }
-        return
+    });
+}
+
+function copyCNAME () {
+    ncp('CNAME', 'docs/CNAME', function (err) {
+        if (err) {
+            return console.error(err);
+        }
     });
 }
 
 function editForProduction () {
     console.log('Preparing files for github pages');
 
-    fs.readFile('docs/index.html', 'utf-8', function(err, data){
+    fs.readFile('docs/index.html', 'utf-8', function (err, data) {
         if (err) throw err;
 
         var newValue = data.replace(/src=\//g, 'src=');
 
         fs.writeFile('docs/index.html', newValue, 'utf-8', function (err) {
             if (err) throw err;
-            fs.readFile('docs/index.html', 'utf-8', function(err, data){
+            fs.readFile('docs/index.html', 'utf-8', function (err, data) {
                 if (err) throw err;
                 newValue = data.replace(/href=\//, 'href=');
                 fs.writeFile('docs/index.html', newValue, 'utf-8', function (err) {
-                    if(fs.existsSync('CNAME')) {
+                    if (fs.existsSync('CNAME')) {
                         copyCNAME()
                     }
+                    if (fs.existsSync('404.html')) {
+                        copy404()
+                    }
                     console.log('Finished! production build is ready for gh-pages');
-                });;
+                });
             });
         });
     })
 }
 
-function runBuild() {
+function runBuild () {
     // Create development build
     console.log('Creating production build');
 
@@ -45,10 +55,10 @@ function runBuild() {
          // Move the dist folder to docs for gh-pages
         ncp.limit = 16;
 
-        ncp('dist', 'docs', function (err)      {
+        ncp('dist', 'docs', function (err) {
             if (err) {
                 return console.error(err);
-            }else {
+            } else {
                 console.log('Build Complete.');
                 path = 'dist';
 
@@ -69,6 +79,6 @@ if (fs.existsSync('docs')) {
     rimraf(path, function () {
         runBuild();
     });
-}else {
+} else {
     runBuild();
 }
