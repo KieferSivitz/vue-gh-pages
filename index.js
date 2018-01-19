9474ad14985ab4233788dd8840038b6cd61b3178
kiefersivitz <kmsivitz@gmail.com>
2018-01-18 19:07:29 -0500

    renaming variables for clarity

--- index.js
+++ index.js
@@ -49,14 +49,14 @@ function editForProduction () {
     fs.readFile('docs/index.html', 'utf-8', function (err, data) {
         if (err) throw err;
 
-        var newValue = data.replace(/src=\//g, 'src=');
+        var replace_href_tags = data.replace(/href=\//g, 'href=');
+        var replace_src_tags = data.replace(/src=\//g, 'src=');
 
-        fs.writeFile('docs/index.html', newValue, 'utf-8', function (err) {
+        fs.writeFile('docs/index.html', replace_src_tags, 'utf-8', function (err) {
             if (err) throw err;
             fs.readFile('docs/index.html', 'utf-8', function (err, data) {
                 if (err) throw err;
-                var newValue2 = data.replace(/href=\//g, 'href=');
-                fs.writeFile('docs/index.html', newValue2, 'utf-8', function (err) {
+                fs.writeFile('docs/index.html', replace_href_tags, 'utf-8', function (err) {
                     if (err) {
                         console.error(err);
                     } else {
