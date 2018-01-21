fad487e62490713acf4970f6b26e61f81fc0eb31
kiefersivitz <kmsivitz@gmail.com>
2018-01-21 16:23:24 -0500

    creating eslintrc and addint it to .gitignore

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 let ncp = require('ncp').ncp;
 let fs = require('fs');
-let exec = require('child_process').exec;
+let execSync = require('child_process').execSync;
 let rimraf = require('rimraf');
 let ghpages = require('gh-pages');
 let path = require('path');
@@ -18,6 +18,7 @@ function pushToGhPages () {
         if (err) {
             console.log('Push to remote failed, please double check that the homepage field in your package.json links to the correct repository.')
             console.log('The build has completed but has not been pushed to github.')
+            return console.error(err);
         } else {
             console.log('Finished! production build is ready for gh-pages');
             console.log('Pushed to gh-pages branch')
@@ -25,28 +26,19 @@ function pushToGhPages () {
     });
 }
 
-function copy404 () {
-    ncp('404.html', 'docs/404.html', function (err) {
-        if (err) {
-            console.error(err);
-        }
+function copyFiles (originalFile, newFile, callback) {
+    ncp.limit = 16;
+    ncp(originalFile, newFile, function (err) {
+        if (err) { return console.error(err); }
+        if (typeof(callback) !== "undefined") { callback(); }
     });
 }
-
-function copyCNAME () {
-    ncp('CNAME', 'docs/CNAME', function (err) {
-        if (err) {
-            console.error(err);
-        }
-    });
-}
-
 
 function editForProduction () {
     console.log('Preparing files for github pages');
 
     fs.readFile('docs/index.html', 'utf-8', function (err, data) {
-        if (err) throw err;
+        if (err) { return console.error(err); }
 
         let replace_href_and_src_tags = data.replace(/(src|href)=\//g, '$1=')
         fs.writeFileSync('docs/index.html', replace_href_and_src_tags)
@@ -60,30 +52,23 @@ function checkIfYarn () {
 }
 
 function runBuild () {
-    console.log('Creating production build...');
-
     const packageManagerName = checkIfYarn() ? 'yarn' : 'npm'
 
-    exec(`${packageManagerName} run build`, function () {
-        ncp.limit = 16;
+    execSync(`${packageManagerName} run build`, {stdio:[0,1,2]});
 
-        ncp('dist', 'docs', function (err) {
-            if (err) {
-                return console.error(err);
+    copyFiles('dist', 'docs', function () {
+        console.log('Build Complete.');
+        const pathToBuild = 'dist';
+        rimraf(pathToBuild, function () {
+            if (fs.existsSync('CNAME')) {
+                copyFiles('CNAME', 'docs/CNAME')
             }
-            console.log('Build Complete.');
-            const pathToBuild = 'dist';
-            rimraf(pathToBuild, function () {
-                if (fs.existsSync('CNAME')) {
-                    copyCNAME()
-                }
-                if (fs.existsSync('404.html')) {
-                    copy404()
-                }
-                editForProduction()
-            });
+            if (fs.existsSync('404.html')) {
+                copyFiles('404.html', 'docs/404.html')
+            }
+            editForProduction()
         });
-    }).stderr.pipe(process.stderr);
+    });
 }
 
 if (fs.existsSync('docs')) {
