2532b1beddc75ff992b59c8154405469f32f349c
kiefersivitz <kmsivitz@gmail.com>
2018-01-21 15:20:35 -0500

    Creating eslintrc

--- index.js
+++ index.js
@@ -39,10 +39,8 @@ function editForProduction () {
     fs.readFile('docs/index.html', 'utf-8', function (err, data) {
         if (err) { return console.error(err); }
 
-        let replace_src_tags = data.replace(/src=\//g, 'src=');
-        let replace_href_tags = data.replace(/href=\//g, 'href=');
-        fs.writeFileSync('docs/index.html', replace_src_tags)
-        fs.writeFileSync('docs/index.html', replace_href_tags)
+        let replace_href_and_src_tags = data.replace(/(src|href)=\//g, '$1=');
+        fs.writeFileSync('docs/index.html', replace_href_and_src_tags);
         if (repository !== null) { pushToGhPages(); }
     });
 }
