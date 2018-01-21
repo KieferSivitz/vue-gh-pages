038a1af92a8a317d1398b5eae2dce5920893bd23
kiefersivitz <kmsivitz@gmail.com>
2018-01-21 15:35:47 -0500

    Changing from exec to execSync

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 let ncp = require('ncp').ncp;
 let fs = require('fs');
-let exec = require('child_process').exec;
+let execSync = require('child_process').execSync;
 let rimraf = require('rimraf');
 let ghpages = require('gh-pages');
 let path = require('path');
@@ -27,6 +27,7 @@ function pushToGhPages () {
 }
 
 function copyFiles (originalFile, newFile, callbck) {
+    ncp.limit = 16;
     ncp(originalFile, newFile, function (err) {
         if (err) { return console.error(err); }
         if (callback) { callback(); }
@@ -54,23 +55,21 @@ function runBuild () {
 
     const packageManagerName = checkIfYarn() ? 'yarn' : 'npm'
 
-    exec(`${packageManagerName} run build`, function () {
-        ncp.limit = 16;
+    execSync(`${packageManagerName} run build`);
 
-        copyFiles('dist', 'docs', function () {
-            console.log('Build Complete.');
-            const pathToBuild = 'dist';
-            rimraf(pathToBuild, function () {
-                if (fs.existsSync('CNAME')) {
-                    copyFiles('CNAME', 'docs/CNAME')
-                }
-                if (fs.existsSync('404.html')) {
-                    copyFiles('404.html', 'docs/404.html')
-                }
-                editForProduction()
-            });
+    copyFiles('dist', 'docs', function () {
+        console.log('Build Complete.');
+        const pathToBuild = 'dist';
+        rimraf(pathToBuild, function () {
+            if (fs.existsSync('CNAME')) {
+                copyFiles('CNAME', 'docs/CNAME')
+            }
+            if (fs.existsSync('404.html')) {
+                copyFiles('404.html', 'docs/404.html')
+            }
+            editForProduction()
         });
-    }).stderr.pipe(process.stderr);
+    });
 }
 
 if (fs.existsSync('docs')) {
