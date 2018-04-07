03c077d6ca7ac895731ba6ebf9bb50f5ccc1aeee
kiefersivitz <kmsivitz@gmail.com>
2018-04-07 13:35:46 -0400

    Changing all double-quotes to single-quotes for consistency

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 #!/usr/bin/env node
 
-"use strict";
+'use strict';
 
-const execSync = require("child_process").execSync;
-const fs = require("fs");
-const ghpages = require("gh-pages");
-const ncp = require("ncp").ncp;
-const packageJson = require("../../package.json");
-const path = require("path");
-const repository = packageJson["homepage"] || null;
-const webpackSimpleTemplate = packageJson["wst"] || null;
-const rimraf = require("rimraf");
+const execSync = require('child_process').execSync;
+const fs = require('fs');
+const ghpages = require('gh-pages');
+const ncp = require('ncp').ncp;
+const packageJson = require('../../package.json');
+const path = require('path');
+const repository = packageJson['homepage'] || null;
+const webpackSimpleTemplate = packageJson['wst'] || null;
+const rimraf = require('rimraf');
 
 console.time('Deployment Time');
 
 function pushToGhPages() {
-    ghpages.publish("docs", {
-            "branch": "master",
-            "dest": "docs",
-            "repo": repository + ".git"
+    ghpages.publish('docs', {
+            'branch': 'master',
+            'dest': 'docs',
+            'repo': repository + '.git'
         },
         function(error) {
             if (error) {
-                console.log("Push to remote failed, please double check that the homepage field in your package.json links to the correct repository.");
-                console.log("The build has completed but has not been pushed to github.");
+                console.log('Push to remote failed, please double check that the homepage field in your package.json links to the correct repository.');
+                console.log('The build has completed but has not been pushed to github.');
                 return console.error(error);
             }
-            console.log("The production build is ready and has been pushed to gh-pages branch.");
+            console.log('The production build is ready and has been pushed to gh-pages branch.');
             removeDocsDirectory();
         }
     );
@@ -47,27 +47,27 @@ function copyFiles(originalFile, newFile, callback) {
         if (error) {
             return console.error(error);
         }
-        if (typeof(callback) !== "undefined") {
+        if (typeof(callback) !== 'undefined') {
             return callback();
         }
     });
 }
 
 function editForProduction() {
-    console.log("Preparing files for github pages");
-    if (!fs.existsSync("docs/index.html")) {
-        fs.createReadStream("index.html").pipe(fs.createWriteStream("docs/index.html"));
+    console.log('Preparing files for github pages');
+    if (!fs.existsSync('docs/index.html')) {
+        fs.createReadStream('index.html').pipe(fs.createWriteStream('docs/index.html'));
     } 
-    fs.readFile("docs/index.html", "utf-8", function(error, data) {
+    fs.readFile('docs/index.html', 'utf-8', function(error, data) {
         if (error) {
             return console.error(error);
         }
-        const removeLeadingSlash = data.replace(/(src|href)=\//g, "$1=");
+        const removeLeadingSlash = data.replace(/(src|href)=\//g, '$1=');
         let removeWebpackSimpleTemplate = removeLeadingSlash;
         if (webpackSimpleTemplate) {
-            removeWebpackSimpleTemplate = data.replace(webpackSimpleTemplate, "");
+            removeWebpackSimpleTemplate = data.replace(webpackSimpleTemplate, '');
         } 
-        fs.writeFileSync("docs/index.html", removeWebpackSimpleTemplate);
+        fs.writeFileSync('docs/index.html', removeWebpackSimpleTemplate);
         if (repository !== null) {
             pushToGhPages();
         }
@@ -75,17 +75,17 @@ function editForProduction() {
 }
 
 function checkIfYarn() {
-    return fs.existsSync(path.resolve("./" || process.cwd(), "yarn.lock"));
+    return fs.existsSync(path.resolve('./' || process.cwd(), 'yarn.lock'));
 }
 
 function runBuild() {
-    const packageManagerName = checkIfYarn() ? "yarn" : "npm";
-    execSync(`${packageManagerName} run build`, { "stdio": [0, 1, 2] });
-    copyFiles("dist", "docs", function() {
-        console.log("Build Complete.");
-        const pathToBuild = "dist";
+    const packageManagerName = checkIfYarn() ? 'yarn' : 'npm';
+    execSync(`${packageManagerName} run build`, { 'stdio': [0, 1, 2] });
+    copyFiles('dist', 'docs', function() {
+        console.log('Build Complete.');
+        const pathToBuild = 'dist';
         rimraf(pathToBuild, function() {
-            const filesToInclude = ["CNAME", "favicon.ico", "404.html"];
+            const filesToInclude = ['CNAME', 'favicon.ico', '404.html'];
             filesToInclude.forEach((file) => {
                 if (fs.existsSync(file)) {
                     copyFiles(file, `docs/${file}`);
@@ -96,8 +96,8 @@ function runBuild() {
     });
 }
 
-if (fs.existsSync("docs")) {
-    const pathToDocs = "docs";
+if (fs.existsSync('docs')) {
+    const pathToDocs = 'docs';
     rimraf(pathToDocs, function() {
         runBuild();
     });
